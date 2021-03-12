aa242262d75d627bfaa96ef96e624e01d3e91e4a
Luan Marques <luanmarques1789@gmail.com>
2021-03-12 11:47:21 -0300

    Initial commit

---
+++ conditionals/compostCondition.js
@@ -0,0 +1,6 @@
+var country = 'Brasil'
+if(country != 'Brasilw'){
+    console.log('Você é estrangeiro!')
+} else {
+    console.log('Você é brasileiro!')
+}
---
+++ conditionals/simpleCondition.js
@@ -0,0 +1,8 @@
+var vel = 90.9
+console.log(`Você está dirigindo a ${vel} km/h}`)
+
+console.log('Use sempre oi cinto de segurança!')
+
+if (vel > 80){ // Condição simples
+    console.log('Você ultrapassou o limite de velocidade. MULTADO!')
+}
