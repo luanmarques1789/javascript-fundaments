613fecf77adf81e7fb4146ab5948571600eeb55b
Luan Matheus Marques <luanmarques1789>
2021-03-20 14:53:29 -0300

    Loops were added in the code

---
+++ looping/loops.js
@@ -0,0 +1,20 @@
+
+// // While
+// var cont = 0;
+// while (cont < 3) {
+//     console.log(`cont = ${cont}`);
+//     cont++; // Isso equivale a cont = cont + 1
+// }
+
+// // Do-While
+// var k = 0;
+// do {
+//     console.log(typeof (""), `(${k})`);
+//     k++;
+// } while (k < 5);
+
+// For looping
+for (var i = 0; i < 3; i++) { // for (início; teste; incremento)
+    console.log(i);
+}
+
