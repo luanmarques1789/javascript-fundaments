c604d9190751b21d3d2c81b2cc6548b0e14798c3
Luan Matheus Marques <luanmarques1789>
2021-03-19 23:50:20 -0300

    Replacing
    actualTime replaced for currentTime

--- practice/daytime/script.js
+++ practice/daytime/script.js
@@ -7,24 +7,24 @@ function loadPhoto() {
     var img = document.querySelector("img#img");
 
     // Getting daytime by client's computer
-    var actual = new Date();
-    var actualTime = actual.getHours();
-    console.log(actualTime);
+    var current = new Date();
+    var currentTime = current.getHours();
+    console.log(currentTime);
 
-    msg.innerHTML = `It's ${actualTime} hours now!`;
-    if (actualTime >= 6 && actualTime < 12) {
+    msg.innerHTML = `It's ${currentTime} hours now!`;
+    if (currentTime >= 6 && currentTime < 12) {
         // Good morning
         msg.innerHTML += "<p>Good morning!<p>" // Se faz necessário o parágrafo para começar em uma nova linha
         img.src = "img/manha.jpg";
         document.body.style.backgroundColor = "#ffd580";
 
-    } else if (actualTime >= 12 && actualTime < 18) {
+    } else if (currentTime >= 12 && currentTime < 18) {
         // Good afternoon
         msg.innerHTML += "<p>Good afternoon!<p>";
         img.src = "img/tarde.jpg";
         document.body.style.backgroundColor = "#b9846f";
 
-    } else if (actualTime <= 23) {
+    } else if (currentTime <= 23) {
         // Good evening
         msg.innerHTML += "<p>Good evening!<p>";
         img.src = "img/noite.jpg";
