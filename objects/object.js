b1dbfa0b717edf1724723dd85990cabb7be28a27
Luan Matheus Marques <luanmarques1789>
2021-04-03 15:57:39 -0300

    New changes
    New changes made in the folders dom and counter
    New file added: object.js

---
+++ objects/object.js
@@ -0,0 +1,10 @@
+var person = {
+  firstName: "Luan",
+  lastName: "Marques",
+  age: 17,
+  eyeColor: "brown",
+};
+
+console.log("My object:", person);
+console.log(`My eyes color is ${person.eyeColor}`);
+console.log(`My name is ${person.firstName}`);
--- practice/counter/script.js
+++ practice/counter/script.js
@@ -1,31 +1,35 @@
-
-
 /**
- * OBJETIVO: Testar a estrutura de repetição 
+ * OBJETIVO: Testar a estrutura de repetição
  */
 
 function countNumbers() {
-    // Se faz necessário o uso do objeto Number, bem como o atributo value
-    let inVal = Number(document.querySelector('input#inVal').value);
-    let finVal = Number(document.querySelector('input#finVal').value);
-    let incr = Number(document.querySelector('input#incr').value);
-    let res = document.querySelector('div#res');
+  // Se faz necessário o uso do objeto Number, bem como o atributo value
+  let initVal = Number(document.querySelector("input#initVal").value);
+  let finVal = Number(document.querySelector("input#finVal").value);
+  let incr = Number(document.querySelector("input#incr").value);
+  let res = document.querySelector("div#res");
 
-    console.log("inVal =", inVal);
-    console.log("finVal =", finVal);
-    console.log("incr =", incr);    
+  console.log("initVal =", initVal);
+  console.log("finVal =", finVal);
+  console.log("incr =", incr);
 
-    if (inVal == 0 || finVal == 0 || incr == 0) {
-        alert("There are empty boxes. Please, fill them.");
+  if (initVal == 0 || finVal == 0 || incr == 0) {
+    alert("There are empty boxes. Please, fill them.");
+  } else {
+    res.innerHTML = "<strong>Counting...</strong>";
 
+    if (initVal > finVal) {
+      for (let i = initVal; i >= finVal; i -= incr) {
+        res.innerHTML += ` ${i} \u{1F449}`;
+        console.log(i);
+      }
     } else {
-        res.innerHTML = "Counting...";
-        for (let i = inVal; i < finVal; i += incr) {
-            res.innerHTML += ` ${i} \u{1F449}`;
-            console.log(i);
-        }
-
-        res.innerHTML += `\u{1F3C1}`;
+      for (let i = initVal; i <= finVal; i += incr) {
+        res.innerHTML += `${i} \u{1F449}`;
+        console.log(i);
+      }
     }
 
+    res.innerHTML += `\u{1F3C1}`;
+  }
 }
