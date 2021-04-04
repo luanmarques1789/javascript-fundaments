918a5c91cc8ab1435261c68f9bb77f0e38aaae87
Luan Matheus Marques <luanmarques1789>
2021-04-04 15:02:10 -0300

    Method getter and setter

--- objects/object.js
+++ objects/object.js
@@ -3,8 +3,19 @@ var person = {
   lastName: "Marques",
   age: 17,
   eyeColor: "brown",
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+  set fullName(value) {
+    const parts = value.split(" ", 2);
+    this.firstName = parts[0];
+    this.lastName = `${parts[1]} ${parts[2]}`;
+  },
 };
 
-console.log("My object:", person);
+console.log(`My object: ${person}`);
 console.log(`My eyes color is ${person.eyeColor}`);
-console.log(`My name is ${person.firstName}`);
+console.log(person.fullName);
+
+person.fullName = "Carlos Rodrigues Maia";
+console.log(`New name: ${person.fullName}`);
