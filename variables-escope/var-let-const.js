eb0a43883eb1bb8e3105aaf80b48b05a5739d8e7
Luan Matheus Marques <luanmarques1789>
2021-04-18 14:55:46 -0300

    Scope of variables

---
+++ variables-escope/var-let-const.js
@@ -0,0 +1,13 @@
+function imprimirIdade() {
+  const idade = 60;
+  console.log("Idade:", idade);
+  if (true) {
+    console.log("if da function:", idade);
+    var nome = "Luan";
+  }
+  console.log("Fora do if:", nome);
+}
+console.log("_______________________________");
+imprimirIdade();
+
+// console.log("Fora da função:", idade);
---
+++ variables-escope/variables.md
@@ -0,0 +1,9 @@
+# DIFERENÇAS ENTRE VAR, LET E CONST
+
+### VAR
+
+O escopo do var vaza para fora de testes condicionais e loops, como if, else, while, do while, for e entre outros...
+
+### LET
+
+### CONST
